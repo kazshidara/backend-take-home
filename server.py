7ab3f0801111ced4d07f9cbdff89221b20f87ef6
Kazune Shidara <kazshidara@ucdavis.edu>
2019-09-20 23:26:55 +0000

    Initial commit of take home challenge

--- server.py
+++ server.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/json')
+def api_call():
+    return render_template('index.html')
+
+
+@app.route('/show_posts')
+def show_user_posts():
+    return render_template('index.html')
